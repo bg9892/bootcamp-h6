f2a5d353cb577b81943a342a1c07cfc915293419
Bradley <bg9892@gmail.com>
2020-03-13 20:59:07 -0500

    add email, tests, and questions to markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -5,12 +5,15 @@ function generateMarkdown(data, inquirerArray) {
 
 <img src="${data.avatar_url}" height="150px" />
 
+[![Generic badge](https://img.shields.io/badge/Contact_at-${data.email}-<COLOR>.svg)](https://shields.io/)
 ## Index
 1. [Description](#description)
 2. [Installation](#installation)
 3. [Usage](#usage)
 4. [License](#license)
 5. [Contribuitng](#contributing)
+6. [Tests](#tests)
+7. [Questions] (#tests)
 <a name="description"></a>
 ### Description
 ${inquirerArray.description}
@@ -26,6 +29,12 @@ This repo is protected under ${inquirerArray.license} licensing.
 <a name="contributing"></a>
 ### Contributing
 ${inquirerArray.contributing}
+### Tests
+<a name="tests"></a>
+${inquirerArray.tests}
+### Questions
+<a name="questions"></a>
+${inquirerArray.questions}
 `;
 }
 
