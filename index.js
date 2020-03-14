8af8bee1927052ff6c88eb6de2abd83332ec2256
Bradley <bg9892@gmail.com>
2020-03-13 19:44:40 -0500

    changed generateMarkdown require path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("generateMarkdown");
 
 const questions = [
     {
